9a5e2b6eaea2c167c01d73ac83f3a05c51997089
Kevin Duarte <duartekevin91@gmail.com>
2018-10-02 19:53:00 -0400

    add feature to enter room

--- client/store/room.js
+++ client/store/room.js
@@ -2,16 +2,18 @@ import axios from 'axios'
 
  const GET_ROOM_DATA = 'GET_ROOM_DATA'
 
- const gotRoomData = (roomData) => ({type:GET_ROOM_DATA, roomData })
+ export const gotRoomData = (roomData) => ({type:GET_ROOM_DATA, roomData })
+
 
  const defaultRoom = {}
 
  export const getRoomData = (content) => {
-  console.log(content)
+   let {userId, roomId} = content
+   console.log({userId, roomId})
   return async dispatch => {
     try {
       const res = await axios.post(
-        `/api/rooms/room`, content
+        `/api/rooms/room`, {userId, roomId}
       )
       console.log(res.data)
       dispatch(gotRoomData(res.data))
