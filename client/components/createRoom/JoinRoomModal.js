775809b559845110a73bf4a449595a6d1ef32189
Matthew Hoover <hooventud29@gmail.com>
2018-10-10 16:50:26 -0400

    create room success leads to page refresh

--- client/components/createRoom/JoinRoomModal.js
+++ client/components/createRoom/JoinRoomModal.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {setPage} from '../../store'
 import {withStyles} from '@material-ui/core/styles'
 import Modal from '@material-ui/core/Modal'
 import {CopyToClipboard} from 'react-copy-to-clipboard'
@@ -7,17 +8,17 @@ import {joinroommodalstyles} from './styles/Material-UI-Style'
 import getModalStyleFunc from './utils/ModalComponent'
 
 class JoinRoomModal extends React.Component {
-  constructor(){
+  constructor() {
     super()
-    this.state={
+    this.state = {
       copied: false
     }
     this.onCopy = this.onCopy.bind(this)
   }
 
   onCopy = () => {
-    this.setState({copied: true});
-  };
+    this.setState({copied: true})
+  }
 
   render() {
     const {classes} = this.props
